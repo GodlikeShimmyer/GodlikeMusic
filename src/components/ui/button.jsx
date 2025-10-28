228770eae41337868c639896fad5556caaa7f675
GodlikeShimmyer <iggomak1@gmail.com>
2025-10-28 01:52:38 -0400

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 export function Button({ children, className = "", ...props }) {
   return (
-    <button
-      {...props}
-      className={`px-4 py-2 rounded-lg bg-cyan-600 hover:bg-cyan-700 transition text-white font-semibold ${className}`}
-    >
+    <button {...props} className={`px-3 py-2 rounded-md bg-cyan-600 hover:bg-cyan-700 ${className}`}>
       {children}
     </button>
   );
