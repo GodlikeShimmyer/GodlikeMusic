8f688896e2cacd22c95e09766b5f2cc2e684f767
GodlikeShimmyer <iggomak1@gmail.com>
2025-10-28 01:52:49 -0400

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-export function Input({ className = "", ...props }) {
-  return (
-    <input
-      {...props}
-      className={`px-3 py-2 rounded-lg bg-[#1b1b1b] border border-transparent focus:border-cyan-500 outline-none text-white w-full ${className}`}
-    />
-  );
+export function Input(props) {
+  return <input {...props} className="px-3 py-2 rounded-md bg-[#111] text-white w-full" />;
 }
