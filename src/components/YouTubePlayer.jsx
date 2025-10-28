1999cd8afe459937f98bdb7d032e3444217b510f
GodlikeShimmyer <iggomak1@gmail.com>
2025-10-28 02:05:29 -0400

    Update YouTubePlayer.jsx

--- src/components/YouTubePlayer.jsx
+++ src/components/YouTubePlayer.jsx
@@ -1,22 +1,23 @@
-// src/components/YouTubePlayer.jsx
-import React from "react";
+import React, { useEffect, useRef } from 'react';
+import YouTube from 'react-youtube';
 
-const YouTubePlayer = ({ videoId }) => {
-  if (!videoId) return null;
+export default function YouTubePlayer({ videoId, isPlaying }) {
+  const playerRef = useRef();
+
+  useEffect(() => {
+    if (playerRef.current) {
+      if (isPlaying) playerRef.current.internalPlayer.playVideo();
+      else playerRef.current.internalPlayer.pauseVideo();
+    }
+  }, [isPlaying]);
 
   return (
-    <div className="w-full h-64 md:h-96 bg-black flex justify-center items-center">
-      <iframe
-        width="100%"
-        height="100%"
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=1`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+    <div className="hidden">
+      <YouTube
+        videoId={videoId}
+        opts={{ playerVars: { autoplay: 0, controls: 0 } }}
+        ref={playerRef}
+      />
     </div>
   );
-};
-
-export default YouTubePlayer;
+}
