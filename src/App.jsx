a769ee40fcfaa9879270a36e7be6e22598707653
GodlikeShimmyer <iggomak1@gmail.com>
2025-10-28 02:05:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import React from "react";
-import Sidebar from "@/components/Sidebar";
-import PlayerBar from "@/components/PlayerBar";
-import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
-import Search from "@/pages/Search";
-import VideoPage from "@/pages/VideoPage";
-import Library from "@/pages/Library";
-import LikedSongs from "@/pages/LikedSongs";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Library from './pages/Library';
+import Album from './pages/Album';
+import Playlist from './pages/Playlist';
+import LikedSongs from './pages/LikedSongs';
+import Artist from './pages/Artist';
+import PlayerBar from './components/PlayerBar';
+import { mockSongs } from './data/mockSongs';
 
 export default function App() {
+  const [currentSong, setCurrentSong] = useState(mockSongs[0]);
+
   return (
-    <div className="app flex h-screen bg-[#09101a] text-white">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <main className="flex-1 overflow-auto">
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 relative min-h-screen bg-gray-900">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/video/:id" element={<VideoPage />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/liked" element={<LikedSongs />} />
+            <Route path="/" element={<Home setCurrentSong={setCurrentSong} />} />
+            <Route path="/library" element={<Library setCurrentSong={setCurrentSong} />} />
+            <Route path="/album/:id" element={<Album setCurrentSong={setCurrentSong} />} />
+            <Route path="/playlist/:id" element={<Playlist setCurrentSong={setCurrentSong} />} />
+            <Route path="/likedsongs" element={<LikedSongs setCurrentSong={setCurrentSong} />} />
+            <Route path="/artist/:id" element={<Artist setCurrentSong={setCurrentSong} />} />
           </Routes>
-        </main>
-        <PlayerBar />
+        </div>
+        <PlayerBar currentSong={currentSong} />
       </div>
-    </div>
+    </Router>
   );
 }
